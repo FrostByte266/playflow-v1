4312b3cb543030942fc36e98fcc79fa3b999e396
William Grigor <bjgrigor@gmail.com>
2022-06-05 17:12:36 -0500

    Added user select screen

---
+++ src/components/UserInfo.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let employeeID: number
+    export let employeeName: string
+</script>
+
+<div class="w-full h-full flex flex-col">
+    <div class="rounded-full bg-gray-500 p-5">
+        <svg xmlns="http://www.w3.org/2000/svg" class="" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+          </svg>
+    </div>
+    <h1 class="text-xl">{String(employeeID).padStart(9,'0')}</h1>
+    <h1 class="text-lg">{employeeName}</h1>
+</div>
--- src/components/UserSelect.svelte
+++ src/components/UserSelect.svelte
@@ -1,11 +1,29 @@
 <script>
-
+    import UserInfo from './UserInfo.svelte'
+    const storeNumber = 795
+    const users = [
+        {
+            employeeID: 445324,
+            employeeName: "John Doe"
+        },
+        {
+            employeeID: 424561,
+            employeeName: "Bob Smith"
+        },
+        {
+            employeeID: 347653,
+            employeeName: "Sarah Lynn"
+        }
+    ]
 </script>
-<div class="bg-gray-400 h-screen ">
+<div class="bg-gray-300 h-screen ">
     <div class="h-full flex flex-col items-center justify-center">
-        <div class="flex flex-col align-center items-center w-1/3 h-1/3 bg-gray-500 rounded-lg">
-            <h1 class="text-3xl">Hello there</h1>
-            <div class="w-full flex justify-around items-center">
+        <div class="p-5 flex flex-col align-center items-center bg-gray-400 rounded-lg">
+            <h1 class="text-3xl">Welcome #{storeNumber}. Please select a user</h1>
+            <div class="w-full p-5 gap-5 flex justify-around items-center">
+                {#each users as user (user.employeeID)}
+                    <UserInfo {...user} />
+                {/each}
             </div>
         </div>
     </div>
