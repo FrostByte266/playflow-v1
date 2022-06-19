79b33882eaa026c70225c9e6ddf1f11e7b033695
William Grigor <bjgrigor@gmail.com>
2022-06-19 16:28:35 -0500

    Changed type of PIN to be string to account for leading zeros

--- server/db/models/employee.ts
+++ server/db/models/employee.ts
@@ -5,8 +5,8 @@ import { Roles } from '../types/enums/employee'
 const EmployeeSchema = new Schema<IEmployee>({
     ID: { type: Number, required: true },
     name: { type: String, required: true },
-    pin: { type: Number, required: true },
-    role: {type: String, enum: Object.keys(Roles), default: Roles.CAST_MEMBER}
+    pin: { type: String, required: true },
+    role: {type: String, enum: Object.values(Roles), default: Roles.CAST_MEMBER}
 })
 
 export default model('Employee', EmployeeSchema)
--- server/db/types/employee.d.ts
+++ server/db/types/employee.d.ts
@@ -3,6 +3,6 @@ import { Roles } from './enums/employee'
 export interface IEmployee {
     ID: number,
     name: string,
-    pin: number,
+    pin: string,
     role: Roles
 }
